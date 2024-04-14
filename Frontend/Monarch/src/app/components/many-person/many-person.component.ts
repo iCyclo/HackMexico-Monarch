28223a1d1cdb016a07dcdd0268bc3833eac4cab0
Mictlan <kevinw130501@gmail.com>
2024-04-14 05:03:17 -0600

    [updated]: many persons

--- Frontend/Monarch/src/app/components/many-person/many-person.component.ts
+++ Frontend/Monarch/src/app/components/many-person/many-person.component.ts
@@ -1,21 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, ReactiveFormsModule } from "@angular/forms";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatSelectModule } from "@angular/material/select";
 
 @Component({
-  selector: 'app-many-person',
-  templateUrl: './many-person.component.html',
-  styleUrls: ['./many-person.component.css'],
+  selector: "app-many-person",
+  templateUrl: "./many-person.component.html",
+  styleUrls: ["./many-person.component.css"],
   standalone: true,
-  imports:[MatFormFieldModule, MatInputModule, MatSelectModule, ReactiveFormsModule]
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+  ],
 })
 export class ManyPersonComponent implements OnInit {
+  personsField: FormControl<number> = new FormControl();
+  desabilityPersons: FormControl<number> = new FormControl();
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.desabilityPersons.valueChanges.subscribe();
   }
-
 }
