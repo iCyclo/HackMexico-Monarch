ca510587500c15e955fca78a77b8439215874e67
Mictlan <kevinw130501@gmail.com>
2024-04-13 22:23:08 -0600

    [added]: timeline

--- Frontend/Monarch/src/app/pages/make-itinerary/make-itinerary.component.ts
+++ Frontend/Monarch/src/app/pages/make-itinerary/make-itinerary.component.ts
@@ -1,16 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { TimelineComponent } from '../../components/timeline/timeline.component';
 
 @Component({
   selector: 'app-make-itinerary',
   templateUrl: './make-itinerary.component.html',
   styleUrls: ['./make-itinerary.component.css'],
-  standalone: true
+  standalone: true,
+  imports: [TimelineComponent],
 })
 export class MakeItineraryComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
